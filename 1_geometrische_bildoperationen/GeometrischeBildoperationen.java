b85c391ef9da6121b89bdf71ee55b78fcb745216
InformatikHub <uli.goeppert@kgt.de>
2021-02-23 16:43:00 +0100

    Revert "Datum"
    This reverts commit c3368eb988aceb6e6d95f953625a9ecff7f96405.

--- 1_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ 1_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * z.B. drehen, spiegeln usw.
  *
  * @author Thomas Schaller
- * @version 1.1 (28.11.2021)
+ * @version 1.1 (28.11.2019)
  */
 public class GeometrischeBildoperationen 
 {

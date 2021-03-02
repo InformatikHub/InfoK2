9cf1bbcb151c5491ccb2a459b0d48ca9dc5599e2
Rhogg2102 <blackbecky2002@gmail.com>
2021-03-02 16:50:24 +0100

    Update GeometrischeBildoperationen.java
    Vertikal, Links, Rechts

--- 1_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ 1_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -34,8 +34,59 @@ public class GeometrischeBildoperationen
         neuesBild.setPixelArray(pixelNeu); 
         return neuesBild;
     }
-
     
+    public  Picture spiegelVertikal(Picture originalbild) {
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
 
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                pixelNeu[x][y] = pixel[x][(hoehe-1)-y];
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+    
+    public  Picture dreheLinks(Picture originalbild) {
+        int breite = originalbild.getHeight();
+        int hoehe  = originalbild.getWidth();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                pixelNeu[x][y] = pixel[y][(breite-1)-x];
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+    
+    public  Picture dreheRechts(Picture originalbild) {
+        int breite = originalbild.getHeight();
+        int hoehe  = originalbild.getWidth();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                pixelNeu[x][y] = pixel[(hoehe-1)-y][x];
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
 
 }

0ed055611217a21422036bbea452787bea33665a
VGri02 <78805746+VGri02@users.noreply.github.com>
2021-03-07 16:23:24 +0100

    AB2

---
+++ 1_geometrische_bildoperationen/Punktoperation.java
@@ -0,0 +1,43 @@
+import imp.*;
+import java.awt.Color;
+import java.util.Random;
+
+/**
+ * Algorithmen zur Änderung der Pixelpositionen eines Pictures
+ * z.B. drehen, spiegeln usw.
+ *
+ * @author Thomas Schaller
+ * @version 1.1 (28.11.2019)
+ */
+public class Punktoperation 
+{
+   
+    
+    /** spiegeleHorizontal spiegelt das Bild, so dass rechts und links getauscht werden
+     * @param originalbild Ein Bild (Picture), das gespiegelt werden soll
+     * @return Eine gespiegelte Kopie des Bildes
+     */
+
+  
+    
+    public  Picture graustufenDurchschnitt(Picture originalbild) {
+        int graudurchschnitt=0;
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
+        
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                graudurchschnitt=(pixel[x][y].getRed()+pixel[x][y].getGreen()+pixel[x][y].getBlue());
+                pixelNeu[x][y] = new Color(graudurchschnitt,graudurchschnitt,graudurchschnitt);
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+    
+}

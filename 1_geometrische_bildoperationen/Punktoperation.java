779c8c7309d8c335dbf491f59e6301d1d5cff351
InformatikHub <uli.goeppert@kgt.de>
2021-03-09 15:32:14 +0100

    /3

--- 1_geometrische_bildoperationen/Punktoperation.java
+++ 1_geometrische_bildoperationen/Punktoperation.java
@@ -6,8 +6,8 @@ import java.util.Random;
  * Algorithmen zur Änderung der Pixelpositionen eines Pictures
  * z.B. drehen, spiegeln usw.
  *
- * @author Thomas Schaller
- * @version 1.1 (28.11.2019)
+ * @author Valentin Grießer
+ * @version 9.3.
  */
 public class Punktoperation 
 {
@@ -30,7 +30,7 @@ public class Punktoperation
 
         for(int x=0; x < breite; x++) {
             for(int y=0;y < hoehe; y++) {
-                graudurchschnitt=(pixel[x][y].getRed()+pixel[x][y].getGreen()+pixel[x][y].getBlue());// Durchschnitt: noch durch 3 teilen...
+                graudurchschnitt=((pixel[x][y].getRed()+pixel[x][y].getGreen()+pixel[x][y].getBlue())/3);// Durchschnitt: noch durch 3 teilen...
                 pixelNeu[x][y] = new Color(graudurchschnitt,graudurchschnitt,graudurchschnitt);
             }
         }
